7a827074474e5f7098a9c65f91b19a8e3218d9e1
GRDtechlab <grdtechlab@gmail.com>
2023-08-14 00:05:15 +0530

    Updated cookir to none sameSite

--- controllers/pfm-users.js
+++ controllers/pfm-users.js
@@ -101,7 +101,7 @@ const pfmUserLogin = asyncHandler(async (req, res) => {
     res.cookie("jwt", `${accessToken}split${refreshToken}`, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict", //  Enforce secure cookies & // Prevent CSRF attacks by setting sameSite
+      sameSite: "none", //  Enforce secure cookies & // Prevent CSRF attacks by setting sameSite
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
     res.status(200).json({
