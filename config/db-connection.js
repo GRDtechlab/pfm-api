560d79e9ad10211694c8e8c797ae878178e27851
GRDtechlab <grdtechlab@gmail.com>
2023-08-14 12:33:16 +0530

    Update token name of mongo MONGODB_URI

--- config/db-connection.js
+++ config/db-connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   try {
-    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+    const connect = await mongoose.connect(process.env.MONGODB_URI);
     console.log(
       "PFM Database connected ",
       connect.connection.host,
